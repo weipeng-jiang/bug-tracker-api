ffce86d29168fb10c7f95476832d9509eb31a0f5
Ray2126 <raymondyang@outlook.co.nz>
2020-01-16 17:40:13 +1300

    Get and insert for roles

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+var db = require("./database");
+
+const ENV = process.env.NODE_ENV;
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use("/api/roles", require("./api/roles"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
+
+db.query("SELECT NOW()", (err, res) => {
+  if (err.error) return console.log(err.error);
+  console.log(`PostgreSql connected: ${res[0].now}.`);
+});
+
+module.exports = app;
---
+++ src/models/roles.js
@@ -0,0 +1,23 @@
+const db = require("../database");
+
+class Roles {
+  static retrieveAll(callback) {
+    db.query("SELECT * from roles", (err, res) => {
+      if (err.error) return callback(err);
+      callback(res);
+    });
+  }
+
+  static insert(role_id, role_title, callback) {
+    db.query(
+      "INSERT INTO roles (role_id, role_title) VALUES ($1, $2)",
+      [role_id, role_title],
+      (err, res) => {
+        if (err.error) return callback(err);
+        callback(res);
+      }
+    );
+  }
+}
+
+module.exports = Roles;
