8e4684f7a61c1874a440f393563578042b0e4369
Raymond Yang <raymondyang@Raymonds-MacBook-Pro.local>
2020-02-01 15:02:24 +1300

    completed api endpoint for assignees

--- src/database/index.js
+++ src/database/index.js
@@ -5,31 +5,39 @@ const CONNECTION_STRING =
   "postgres://fpirroxt:bRuA9HLENn3rxNNW_uzFJ2A0bUQw0oEO@rosie.db.elephantsql.com:5432/fpirroxt";
 const SSL = process.env.NODE_ENV === "production";
 
-const pool = new Pool({
-  connectionString: CONNECTION_STRING,
-  ssl: SSL
-});
-
-pool.on("error", (err, client) => {
-  console.error("Unexpected error on idle PostgreSQL client.", err);
-  process.exit(-1);
-});
-
-pool.query = (query, ...args) => {
-  pool.connect((err, client, done) => {
-    if (err) throw err;
-    const params = args.length === 2 ? args[0] : [];
-    const callback = args.length === 1 ? args[0] : args[1];
-
-    client.query(query, params, (err, res) => {
-      done();
-      if (err) {
-        console.log(err.stack);
-        return callback({ error: "Database error." }, null);
-      }
-      callback({}, res.rows);
+class Database {
+  constructor() {
+    this._pool = new Pool({
+      connectionString: CONNECTION_STRING,
+      ssl: SSL
     });
-  });
-};
 
-module.exports = pool;
+    this._pool.on("error", (err, client) => {
+      console.error("Unexpected error on idle PostgreSQL client.", err);
+      process.exit(-1);
+    });
+  }
+
+  query(query, ...args) {
+    this._pool.connect((err, client, done) => {
+      if (err) throw err;
+      const params = args.length === 2 ? args[0] : [];
+      const callback = args.length === 1 ? args[0] : args[1];
+
+      client.query(query, params, (err, res) => {
+        done();
+        if (err) {
+          console.log(err.stack);
+          return callback({ error: "Database error." }, null);
+        }
+        callback({}, res.rows);
+      });
+    });
+  }
+
+  end() {
+    this._pool.end();
+  }
+}
+
+module.exports = new Database();
