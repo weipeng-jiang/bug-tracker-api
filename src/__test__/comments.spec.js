03c8819e4c4347719f66d0fd4cbed9900bac0694
Ray2126 <raymondyang@outlook.co.nz>
2020-04-03 19:30:04 +1300

    refined unit tests

--- src/__test__/comments.spec.js
+++ src/__test__/comments.spec.js
@@ -7,4 +7,10 @@ describe("Testing the comments API", () => {
     expect(response.status).toBe(200);
     done();
   });
+
+  it("tests the status code for the get by comment id endpoint", async done => {
+    const response = await request(app).get("/api/comments/100");
+    expect(response.status).toBe(200);
+    done();
+  });
 });
